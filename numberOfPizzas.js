526a50c6e7abc0f13efe09a6bd5731a6ae48e29f
ARPAN MONDAL <accesstoarpan@gmail.com>
2023-03-18 02:51:55 +0530

    Create

---
+++ numberOfPizzas.js
@@ -0,0 +1,9 @@
+function numberOfPizzas(orders) {
+    let total = 0;
+    for(let i = 0; i < orders.length; i++) {
+        total += orders[i].pizzas;
+    }
+    return total;
+}
+
+module.exports = numberOfPizzas;
